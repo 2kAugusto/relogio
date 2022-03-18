45b91f828c54e2a65eb61b12158a1e09dd05a7df
2kAugusto <101876146+2kAugusto@users.noreply.github.com>
2022-03-18 17:13:55 -0300

    Add files via upload

---
+++ estilo.js
@@ -0,0 +1,9 @@
+//var titulo = window.document.getElementsByTagName(h1)
+
+// var cores = ["blue", "red", "orange", "yellow", "purple", "green"]
+// cores.forEach(cor => {
+//     console.log (cor)
+//        setInterval(() => {
+//     document.querySelector("#titulo").style.color = cor       
+//     }, 2000);
+//     });
